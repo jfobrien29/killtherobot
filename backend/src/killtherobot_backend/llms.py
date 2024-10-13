9be0affe665cfb623179a061e8ef4c890fc6a0a1
Igor Sadalski <igor.sadalski@gmail.com>
2024-10-13 16:23:08 -0400

    add template of curated personalities

--- backend/src/killtherobot_backend/llms.py
+++ backend/src/killtherobot_backend/llms.py
@@ -1,10 +1,12 @@
 from openai import OpenAI
 from typing import Generator
+from utils import Game, Human, Answer, Round
+from collections import defaultdict
 
 def answer_question(
     question: str,
     key: str,
-    personality: str = "helpful assistant",
+    personality: str = "funny",
     temperature: float = 0.7,
     max_tokens: int = 150,
     top_p: float = 1.0,
@@ -13,11 +15,16 @@ def answer_question(
 ) -> Generator[str, None, None]:
     
     client = OpenAI(api_key=key)
+
+    personality_template = {'hilarious': 'hilarious comedian and helpful assistant',
+                            'cynical': 'cynical comedian and helpful assistant',
+                            'funny': 'funny guy with the humor of a 15 year old boy and helpful assistant'}
+
     
     stream = client.chat.completions.create(
         model="gpt-4-0125-preview",
         messages=[
-            {"role": "system", "content": f"You are a {personality}. Respond to the user's questions accordingly."},
+            {"role": "system", "content": f"You are a {personality_template[personality]}.  Listen to my instructions and respond. Think out of the box and be funny. Write like a human would using their phone keyboard. Do not use special characters, emojis, quotes, or punctuation. Only capitalize the first letter of your response.."},
             {"role": "user", "content": question}
         ],
         temperature=temperature,
@@ -28,6 +35,9 @@ def answer_question(
         stream=True,
     )
     
+    content = ""
     for chunk in stream:
         if chunk.choices[0].delta.content is not None:
-            yield chunk.choices[0].delta.content
+            content += chunk.choices[0].delta.content
+
+    return Answer(name="AI", text=content, votes=[])
