32787dde531b1b4588d89fe6a8033967c7b4794f
Jack O'Brien <thejackobrien29@gmail.com>
2024-10-13 22:48:52 -0400

    fix the other join screen

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -5,17 +5,17 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import { InputField } from '@/components/inputField';
-import { useMutation, useQuery } from 'convex/react';
+import { useMutation } from 'convex/react';
 import { api } from '@/convex/_generated/api';
-import { useParams, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 const schema = Yup.object().shape({
-  code: Yup.string().required('Game code is required'),
   name: Yup.string()
     .required('Name is required')
     .min(2, 'Name must be at least 2 characters')
     .max(20, 'Name must be at most 20 characters')
     .matches(/^[A-Za-z\s]+$/, 'Only characters and spaces are allowed'),
+  code: Yup.string().required('Code is required'),
 });
 
 export default function Home() {
@@ -23,8 +23,7 @@ export default function Home() {
     resolver: yupResolver(schema),
   });
   const router = useRouter();
-  const { code } = useParams();
-  const game = useQuery(api.game.get, { code: code as string });
+
   const joinGame = useMutation(api.game.join);
 
   const onSubmit = async (data: { name: string; code: string }) => {
@@ -38,18 +37,21 @@ export default function Home() {
     } catch (e: any) {
       console.error(e);
       console.log('error', e.message);
+
+      methods.setError('code', {
+        message: 'Game not found',
+        type: 'required',
+      });
     }
   };
 
   return (
     <div className="flex flex-col items-center min-h-screen pb-20 gap-16 p-4 w-full justify-between">
       <div className="absolute inset-0 bg-[url('/bg.webp')] bg-cover bg-center opacity-5 z-0"></div>
-
-      <div className="w-full">
+      <div className="w-full relative z-10 flex flex-col items-center min-h-screen gap-16">
         <div className="text-center text-sm text-gray-500 mb-4">
-          <h2 className="text-base font-semibold">{game?.name || 'Kill The Robot'}</h2>
+          <h2 className="text-base font-semibold">Kill The Robot</h2>
         </div>
-        <h1>Booting up Kill The Robot...</h1>
         <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(onSubmit)} className="w-full max-w-md space-y-4">
             <InputField label="Game Code" name="code" />
